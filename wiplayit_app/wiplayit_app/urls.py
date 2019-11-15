5a0e51c21e422d23e388f6f5768a600631bc1e1f
Silasi baloyi <silassibaloy@gmail.com>
2019-11-15 10:01:32 +0200

    More modification in the static files code

--- wiplayit_app/wiplayit_app/urls.py
+++ wiplayit_app/wiplayit_app/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('app_backend.urls.update_urls')),
     path('', include('app_backend.urls.retrieve_urls')),
     path('', include('app_backend.urls.auth_urls')),
+    path('', include('django.contrib.auth.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     re_path(r'.*/', index),
   
