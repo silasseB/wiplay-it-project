137a802220f8530e505825b2485a0604d71ba176
Silasi baloyi <silassibaloy@gmail.com>
2020-03-30 07:08:55 +0200

    Removed authentication files in app_backend directory and created authentication app backend

--- wiplayit_app/wiplayit_app/settings/production.py
+++ wiplayit_app/wiplayit_app/settings/production.py
@@ -7,16 +7,16 @@ from wiplayit_app.settings.common import *
 DEBUG = False
 SITE_ID = 2
 
-ALLOWED_HOSTS = ['valoi.pythonanywhere.com', 'silasi.pythonanywhere.com']
+ALLOWED_HOSTS = ['baloyi.pythonanywhere.com','valoi.pythonanywhere.com', 'silasi.pythonanywhere.com']
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Valoi$wiplayitdb',
-        'USER': 'Valoi',
+        'NAME': 'Baloyi$wiplayitdb',
+        'USER': 'Baloyi',
         'PASSWORD': 'siLasValoi9020$?',
-        'HOST': 'Valoi.mysql.pythonanywhere-services.com',
+        'HOST': 'Baloyi.mysql.pythonanywhere-services.com',
         'PORT': '',
         'OPTIONS': {'charset': 'utf8mb4'},
         
