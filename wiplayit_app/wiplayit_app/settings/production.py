d2b7c922f93ca54019b999b727beda04e0f885e1
Silasi baloyi <silassibaloy@gmail.com>
2020-05-14 10:52:02 +0200

    Fixed more bugs and changed conf for sending emails in backend sittings files

--- wiplayit_app/wiplayit_app/settings/production.py
+++ wiplayit_app/wiplayit_app/settings/production.py
@@ -7,14 +7,23 @@ from wiplayit_app.settings.common import *
 DEBUG = False
 SITE_ID = 3
 
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_HOST_USER     = 'silassibaloy@gmail.com'
+EMAIL_HOST_PASSWORD = 'SilasiBaloyi9020@?'
+
 EMAIL_PORT = 587
 
-
+'''
 EMAIL_HOST = 'wiplayit-com.mail.protection.outlook.com'
 EMAIL_HOST_USER = 'info@wiplayit.com'
 EMAIL_HOST_PASSWORD = 'SilasValoi1990@?'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+'''
+
+
 
 SECURE_SSL_REDIRECT = True
 
