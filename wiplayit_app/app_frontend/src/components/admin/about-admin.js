9be5e42340e7def76a64fa4f530c1a6817796cf4
Silasi baloyi <silassibaloy@gmail.com>
2020-05-20 14:43:47 +0200

    Removed a testing code which was not longer needed

--- wiplayit_app/app_frontend/src/components/admin/about-admin.js
+++ wiplayit_app/app_frontend/src/components/admin/about-admin.js
@@ -129,16 +129,6 @@ class AboutAdminPage extends Component {
 
 export default  MainAppHoc(AboutAdminPage);
 
-let draftContents = {
-                    "blocks":[
-                            {"key": "dau30","text":"", 
-                             "type":"unstyled","depth":0,
-                             "inlineStyleRanges":[],
-                              "entityRanges":[],
-                              "data":{} }
-                            ],"entityMap":{}
-                        }
-
 
 export const AboutAdminComponent = props => {
     let about = props.about;
@@ -146,75 +136,42 @@ export const AboutAdminComponent = props => {
     let isPut    = about && about.length  && true || false;
     let isPost   = about && !about.length && true || false;
     console.log(props ,about)
-    let about_text = about && about.length && about[1].about_text;
-    let aboutIsString = checkType.isString(about_text)
-    console.log(aboutIsString, about_text)
-    if (aboutIsString) {
-        for (var i = about_text.length + 1; i >= 0; i++) {
-           
-           let carecter = about_text[i];
-           if (carecter !== '{') {
-            let blocks = draftContents.blocks[0]
-            let text = blocks.text;
-            text = about_text
-            draftContents.blocks[0].text = about_text
-            console.log(carecter, text, blocks)
-            break
-           }
-        }
-    }
-
     
-    let isString = checkType.isString(draftContents)
-    console.log(draftContents, isString)
-    draftContents = JSON.stringify(draftContents)
-    isString =checkType.isString(draftContents)
-    console.log(draftContents, isString)
-    draftContents = JSON.parse(draftContents);
-
-    isString =checkType.isString(draftContents)
-    console.log(draftContents, isString)
     
     return(
         <div className="about-admin-contents" id="about-admin-contents">
-            
-                
-            <div className="">
-                {about && about.map( (about, index)=>{
-                    let editAboutProps = {
-                            isPost,
-                            isPut,
-                            obj       : about,
-                            objName   : 'About',
-                            className : "edit-about-admin-btn btn-sm",
-                    };
-                    editAboutProps = GetModalLinkProps.props(editAboutProps);
-                    
-                    let editorState = helper.convertFromRaw(about.about_text)
-                    
-                    if (!editorState) return null;
+            {about && about.length && about.map( (about, index)=>{
+                let editAboutProps = {
+                        isPost,
+                        isPut,
+                        obj       : about,
+                        objName   : 'About',
+                        className : "edit-about-admin-btn btn-sm",
+                };
+                editAboutProps = GetModalLinkProps.props(editAboutProps);
+              
+                let editorState = helper.convertFromRaw(about.about_text)
+                if (!editorState) return null;
 
-                    return(
-                        <div key={index}>
-                            <OpenEditorBtn {...editAboutProps}/>
-                            <div className="">
-                                <h2 className="about-info-title">{about.about_title}</h2>
-                            </div>
-
-                            <div>
-                                <Editor
-                                    blockRendererFn={pageMediaBlockRenderer}
-                                    editorState={editorState} 
-                                    readOnly={true} 
-                                />
-                            </div>
-
-                            <div>
-                            </div>
+                return(
+                    <div key={index}>
+                        <OpenEditorBtn {...editAboutProps}/>
+                        <div className="">
+                            <h2 className="about-info-title">{about.about_title}</h2>
                         </div>
-                    )
-                })}
-            </div>
+
+                        <div>
+                            <Editor
+                                blockRendererFn={pageMediaBlockRenderer}
+                                editorState={editorState} 
+                                readOnly={true} 
+                            />
+                        </div>
+                       <div>
+                       </div>
+                    </div>
+                )
+            })}
         </div>
     )
 
