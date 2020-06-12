d39c956ea86e7a4df85141aa788493c24ab52156
Silasi baloyi <silassibaloy@gmail.com>
2020-06-12 10:47:29 +0200

    Updated backend settings

--- wiplayit_app/wiplayit_app/settings/common.py
+++ wiplayit_app/wiplayit_app/settings/common.py
@@ -17,6 +17,7 @@ import datetime
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+print(SECRET_KEY)
 
 
 AUTH_USER_MODEL = 'auth_backend.User'
