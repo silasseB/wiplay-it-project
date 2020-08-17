70fd892118b74f478f46b07d890c1bac1308ae94
Silasi baloyi <silassibaloy@gmail.com>
2020-08-17 08:23:47 +0200

    More bug fixes

--- wiplayit_app/wiplayit_app/settings/production.py
+++ wiplayit_app/wiplayit_app/settings/production.py
@@ -18,6 +18,7 @@ ALLOWED_HOSTS = ['baloyi.pythonanywhere.com','wiplayit.com','www.wiplayit.com']
 
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
+EMAIL_PORT =  587
 
 EMAIL_HOST          = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER     = os.getenv("EMAIL_HOST_USER")
