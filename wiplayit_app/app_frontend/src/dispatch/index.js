4cd2e164c12ffac50505d51139246acf620dc57a
Silasi baloyi <silassibaloy@gmail.com>
2020-09-16 06:11:52 +0200

    More bug fixes

--- wiplayit_app/app_frontend/src/dispatch/index.js
+++ wiplayit_app/app_frontend/src/dispatch/index.js
@@ -20,7 +20,7 @@ export const _GetApi =(useToken, opts={}) =>{
 export function sendMessage(options={}) {
     let useToken = false;
     let opts = {}
-    //const Api    = _GetApi(useToken,{timeout:120000});
+    const Api    = _GetApi(useToken,{timeout:120000});
     
     if(!Api){
         console.log(!Api)
@@ -709,7 +709,7 @@ export function authenticate(params={}){
     let useTokenIsBolean = checkType.isBoolean(useToken)
     useToken  = useTokenIsBolean &&  useToken || false;
 
-    //const Api = _GetApi(useToken, {timeout:120000, requestFor:'authentication'});   
+    const Api = _GetApi(useToken, {timeout:120000, requestFor:'authentication'});   
     if(!Api){
         return  dispatch =>{ 
             dispatch(action.handleError());
